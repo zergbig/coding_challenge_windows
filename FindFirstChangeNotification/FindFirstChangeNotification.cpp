52ae142eb287dc765de2cf39cc5d4e63edf3aea9
Greg11L <104029696+Greg11L@users.noreply.github.com>
2022-04-19 18:22:58 -0400

    Update FindFirstChangeNotification.cpp
    I just added in my code since I have a different version of VS, I thought it might be easier.

--- FindFirstChangeNotification/FindFirstChangeNotification.cpp
+++ FindFirstChangeNotification/FindFirstChangeNotification.cpp
@@ -15,33 +15,33 @@ ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+void                RefreshDirectory(HANDLE&);
 
 HANDLE g_hThreadExit = NULL;
 HANDLE g_readyMutex = nullptr;
 HANDLE g_notificationListMutex = nullptr;
 
 
-void MyThread (void *param)
+void MyThread(void* param)
 {
-	DWORD dwWaitStatus; 
-	HANDLE dwChangeHandles[2]; 
-	 
+	DWORD dwWaitStatus;
+	HANDLE dwChangeHandles[2];
+
 	// Watch the C:\WINDOWS directory for file creation and 
 	// deletion. 
-	 
-	dwChangeHandles[0] = FindFirstChangeNotification( 
+	dwChangeHandles[0] = FindFirstChangeNotification(
 		"C:\\Windows",                 // directory to watch 
 		TRUE,                         // do not watch the subtree 
 		FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_ATTRIBUTES | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_SECURITY); // watch file name changes 
-	 
-	if (dwChangeHandles[0] == INVALID_HANDLE_VALUE) 
+
+	if (dwChangeHandles[0] == INVALID_HANDLE_VALUE)
 	{
 		return;
 	}
-	 
+
 	// Watch the C:\ subtree for directory creation and 
 	// deletion. 
-	 
+
 	dwChangeHandles[1] = g_hThreadExit;
 	DWORD drawListRet = WaitForSingleObject(g_readyMutex, INFINITE);
 
@@ -49,40 +49,55 @@ void MyThread (void *param)
 	// handles and refresh accordingly. 
 	BOOL doneFlag = false;
 	while (doneFlag == false)
-	{ 
-	 
+	{
+
 		// Wait for notification.
-	 
-		dwWaitStatus = WaitForMultipleObjects(2, dwChangeHandles, FALSE, INFINITE); 
-	 
-		switch (dwWaitStatus) 
-		{ 
-		case WAIT_OBJECT_0: 
-	
-		// A file was created or deleted in C:\WINDOWS. 
-		// Refresh this directory and restart the 
-		// change notification. RefreshDirectory is an 
-		// application-defined function. 
-			if ( FindNextChangeNotification(dwChangeHandles[0]) == FALSE ) 
+
+		dwWaitStatus = WaitForMultipleObjects(2, dwChangeHandles, FALSE, INFINITE);
+
+		switch (dwWaitStatus)
+		{
+		case WAIT_OBJECT_0:
+
+			// A file was created or deleted in C:\WINDOWS. 
+			// Refresh this directory and restart the 
+			// change notification. RefreshDirectory is an 
+			// application-defined function. 
+			if (FindNextChangeNotification(dwChangeHandles[0]) != FALSE)
 			{
+				// Note from Greg: I used the dump to see the point of failure.
+				// the main thread was getting stuck in GetReady() waiting for the 
+				// g_readyMutex to be unlocked. This lead me to think that the 
+				// condition for releasing the mutex was not being met. So doneFlag 
+				// became a prime suspect. Since this is a stand-alone application, 
+				// I was lucky enough to be able to use breakpoints and VS's thread 
+				// tracing. I saw that the child thread was indeed getting stuck in this
+				// while, which confirmed my suspicions. I investigated the cases here 
+				// closley and saw that dwChangeHandles[0] == false is not a good return
+				// from FindNextChangeNotification, it would actually indicate a failure
+
 				doneFlag = true;
 			}
-			break; 
-	
-		case WAIT_OBJECT_0 + 1: 
-	
+			else
+			{
+				ExitProcess(GetLastError());
+			}
+			break;
+
+		case WAIT_OBJECT_0 + 1:
+
 			// exit from loop and thread
 			doneFlag = true;
-			break; 
-	
-		default: 
+			break;
+
+		default:
 			// exit from loop and thread
 			doneFlag = true;
 			break;
 		}
 	} // while doneFlag == false
 
-	FindCloseChangeNotification (dwChangeHandles[0]);
+	FindCloseChangeNotification(dwChangeHandles[0]); //greg this should release the mutex 
 	if (drawListRet == WAIT_OBJECT_0 || drawListRet == WAIT_ABANDONED)
 	{
 		ReleaseMutex(g_readyMutex);
@@ -91,9 +106,10 @@ void MyThread (void *param)
 	return;
 }
 
+
 void GetReady()
 {
-	DWORD drawListRet = WaitForSingleObject(g_readyMutex, INFINITE);
+	DWORD drawListRet = WaitForSingleObject(g_readyMutex, INFINITE); //greg here top of stack fail;
 
 	DWORD notificationRet = WaitForSingleObject(g_notificationListMutex, INFINITE);
 
@@ -103,6 +119,7 @@ void GetReady()
 	}
 }
 
+
 void DoneReady()
 {
 	ReleaseMutex(g_readyMutex);
@@ -110,20 +127,20 @@ void DoneReady()
 
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
 {
- 	// TODO: Place code here.
+	// TODO: Place code here.
 	MSG msg;
 	HACCEL hAccelTable;
 
 	g_readyMutex = CreateMutex(NULL, FALSE, NULL);
 	g_notificationListMutex = CreateMutex(NULL, FALSE, NULL);
 
-	DWORD threadId  = 0;
-	g_hThreadExit = CreateEvent (NULL, 0, 0, NULL);
-	HANDLE hThread = CreateThread (NULL, 0, (LPTHREAD_START_ROUTINE)MyThread, NULL, 0, &threadId);
+	DWORD threadId = 0;
+	g_hThreadExit = CreateEvent(NULL, 0, 0, NULL);
+	HANDLE hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)MyThread, NULL, 0, &threadId);
 
 
 
@@ -133,7 +150,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	MyRegisterClass(hInstance);
 
 	// Perform application initialization:
-	if (!InitInstance (hInstance, nCmdShow)) 
+	if (!InitInstance(hInstance, nCmdShow))
 	{
 		return FALSE;
 	}
@@ -141,21 +158,20 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	hAccelTable = LoadAccelerators(hInstance, (LPCTSTR)IDC_FINDFIRSTCHANGENOTIFICATION);
 
 	// Main message loop:
-	while (GetMessage(&msg, NULL, 0, 0)) 
+	while (GetMessage(&msg, NULL, 0, 0))
 	{
-		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) 
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
 	}
 
-	SetEvent (g_hThreadExit);
-	WaitForSingleObject (hThread, INFINITE);
+	SetEvent(g_hThreadExit);
+	WaitForSingleObject(hThread, INFINITE);
 
-	return (int) msg.wParam;
+	return (int)msg.wParam;
 }
-
 
 
 //
@@ -175,22 +191,23 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 {
 	WNDCLASSEX wcex;
 
-	wcex.cbSize = sizeof(WNDCLASSEX); 
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= (WNDPROC)WndProc;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, (LPCTSTR)IDI_FINDFIRSTCHANGENOTIFICATION);
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= (LPCTSTR)IDC_FINDFIRSTCHANGENOTIFICATION;
-	wcex.lpszClassName	= szWindowClass;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, (LPCTSTR)IDI_SMALL);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = (WNDPROC)WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, (LPCTSTR)IDI_FINDFIRSTCHANGENOTIFICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = (LPCTSTR)IDC_FINDFIRSTCHANGENOTIFICATION;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, (LPCTSTR)IDI_SMALL);
 
 	return RegisterClassEx(&wcex);
 }
+
 
 //
 //   FUNCTION: InitInstance(HANDLE, int)
@@ -204,23 +221,24 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   HWND hWnd;
+	HWND hWnd;
 
-   hInst = hInstance; // Store instance handle in our global variable
+	hInst = hInstance; // Store instance handle in our global variable
 
-   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
-   return TRUE;
+	return TRUE;
 }
+
 
 //
 //  FUNCTION: WndProc(HWND, unsigned, WORD, LONG)
@@ -239,10 +257,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	HDC hdc;
 	GetReady();
 
-	switch (message) 
+	switch (message)
 	{
 	case WM_COMMAND:
-		wmId    = LOWORD(wParam); 
+		wmId = LOWORD(wParam);
 		wmEvent = HIWORD(wParam);
 		// Parse the menu selections:
 		switch (wmId)
@@ -272,6 +290,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
+
 // Message handler for about box.
 LRESULT CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -281,7 +300,7 @@ LRESULT CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 		return TRUE;
 
 	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) 
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
 		{
 			EndDialog(hDlg, LOWORD(wParam));
 			return TRUE;
