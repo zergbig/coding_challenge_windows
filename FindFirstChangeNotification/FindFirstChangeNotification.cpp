2e4c6c0c189bf771becc283ff74c996c0bc224d9
Greg11L <104029696+Greg11L@users.noreply.github.com>
2022-04-21 14:03:42 -0400

    Update FindFirstChangeNotification.cpp
    remove unneeded line

--- FindFirstChangeNotification/FindFirstChangeNotification.cpp
+++ FindFirstChangeNotification/FindFirstChangeNotification.cpp
@@ -15,7 +15,6 @@ ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
-void                RefreshDirectory(HANDLE&);
 
 HANDLE g_hThreadExit = NULL;
 HANDLE g_readyMutex = nullptr;
